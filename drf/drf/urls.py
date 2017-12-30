a7b67b445c35e8a144f1c158bc9e8b135769422b
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2017-12-30 12:56:36 +0600

    feat: demo api with built in classes
    >>> Why is this Change Necessary?:
    Needed to understand the whole scenario
    
    >>> This change address the need by:
    By providing the simplest way to make an awesome API
    
    >>> What Side Effects does this change have?
    None
    
    >>> Resource: http://www.django-rest-framework.org/

--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -1,7 +1,34 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from rest_framework import (
+    routers,
+    serializers,
+    viewsets
+)
+
+# serializers define the API representations
+class UserSerializer(serializers.HyperLinkedModelSerializer):
+    class Meta:
+        model = User,
+        fields = (
+            'url',
+            'username',
+            'email',
+            'is_staff'
+        )
+
+# viewsets define the view behavior
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide and easy way of automatically determining the URL Conf
+router = routers.DefaultRouter()
+rotuer.register(r'users', UserViewSet)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'api-auth/', include('rest_framework.urls'))
+    url(r'api-auth/', include('rest_framework.urls')),
+    url(r'^', include(router.urls))
 ]
